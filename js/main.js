c2436233c20277c52cd9a7e996e08b34d9d2dca5
Yuliia Pchelintseva <junoromanova@gmail.com>
2023-01-14 23:37:58 +0000

    readme

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
 const markAllEl = document.querySelector('.mark-read');
 const notificationDivs = document.querySelectorAll('div.notification');
 const quantityOfNotifications = document.querySelector('.number-notifications');
-// const quantityNumber = document.querySelector('.number-notifications').textContent;
+// let quantityNumber = document.querySelector('.number-notifications').textContent;
 
 markAllEl.addEventListener('click', e => {
   for (let i = 0; i < notificationDivs.length; i++) {
     if (notificationDivs[i].classList.contains('unread')) {
       notificationDivs[i].classList.add('read');
       quantityOfNotifications.classList.add('d-none');
+      // quantityOfNotifications.textContent = '0';
     }
   }
 });
